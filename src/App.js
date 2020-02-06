733bd8f5614c1ed4f97a9a763b6edd42ae396788
chen <578159913@qq.com>
2020-02-06 08:58:53 +0800

    react项目build功能初步完成

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                app222
+            </div>
+        )
+    }
+}
+
+
+
+export default App;
